9e2454e8e0d92e57192e96933194927344863f59
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-03-03 12:38:36 +0100

    Add project files.

---
+++ Academy-2024/Repositories/CourseRepository.cs
@@ -0,0 +1,58 @@
+﻿using Academy_2024.Models;
+
+namespace Academy_2024.Repositories
+{
+    public class CourseRepository
+    {
+        private static List<Course> Courses = new List<Course>
+        {
+            new Course{Id=1,Name="Name"}
+
+        };
+
+        public List<Course> GetAll() { return Courses; }
+
+        public Course? GetById(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id) return course;
+            }
+            return null;
+        }
+
+        public void Create(Course data)
+        {
+            if (data == null) return;
+            Courses.Add(data);
+        }
+
+        public Course? Update(int id, Course data)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.Name = data.Name;
+                    return course;
+                }
+            }
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
